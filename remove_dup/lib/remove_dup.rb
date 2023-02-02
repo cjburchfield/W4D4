8a7c910ebbc51e7a0b026a62969cc7298b542812
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-02-02 15:45:21 -0500

    Finishing two_sum

--- remove_dup/lib/remove_dup.rb
+++ remove_dup/lib/remove_dup.rb
@@ -9,3 +9,18 @@ def remove_dup(arr)
 
     duped
 end
+
+def two_sum(arr)
+    pairs = []
+
+    arr.each_with_index do |el1, i1|
+        arr.each_with_index do |el2, i2|
+            if i2 > i1 && el1 + el2 == 0
+                pairs.push([i1, i2])
+            end
+        end
+    end
+
+    pairs
+end
+
--- remove_dup/spec/remove_dup_spec.rb
+++ remove_dup/spec/remove_dup_spec.rb
@@ -2,31 +2,42 @@ require "rspec"
 require "remove_dup"
 
 
-describe "remove_dup" do
-  let(:arr_1) {[1,2,1,3,3]}
-  let(:duped) { remove_dup(arr_1.dup)}
+# describe "remove_dup" do
+#   let(:arr_1) {[1,2,1,3,3]}
+#   let(:duped) { remove_dup(arr_1.dup)}
 
-  #  it "returns a duped arr" do
-  #   expect(remove_dup([1,2,1,3,3])).to eq([1,2,3])
-  # end
+#   it "should only contain unique elements" do
+#     arr_1.each do |ele|
+#      expect(duped.count(ele)).to eq(1)
+#     end
+#   end
 
+#   it "should return a new array that does not mutate the original array" do
+#     expect(arr_1.object_id).not_to eq(duped.object_id)
+#     expect(duped).to be_a(Array)
+#   end
 
+#   it "should not include elements not in the original array" do
+#     expect(duped.all?{|el|arr_1.include?(el)}).to eq(true)
+#   end
+# end
 
-  it "should only contain unique elements" do
-    arr_1.each do |ele|
-     expect(duped.count(ele)).to eq(1)
-    end
+describe "two_sum" do
+  let(:arr_1) {[-1, 0, 2, -2, 1]}
+  let(:pairs) {[[0,4],[2,3]]}
+
+  it "should return a new arrayy" do
+    expect(pairs).to be_a(Array)
   end
 
-  it "should return a new array that does not mutate the original array" do
-    expect(arr_1.object_id).not_to eq(duped.object_id)
-    expect(duped).to be_a(Array)
-  end
-
-  it "should not include elements not in the original array" do
-    expect(duped.all?{|el|arr_1.include?(el)}).to eq(true)
-  end
-
+  it "should have the elements of each subarray sum to zero" do
+      expect(pairs.all? {|subpair| (arr_1[subpair[0]] + arr_1[subpair[1]]) == 0} ).to eq(true)
 end
 
+  it "should have each subarray be sorted dictionary wise" do #check indexes
+    expect(pairs.all? {|subpair| subpair[0] < subpair[1]}).to eq(true)
+  end
+
+
+end
 
